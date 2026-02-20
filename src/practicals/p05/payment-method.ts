3d80812220b09785cdf964c64851ee5ed231064d
unknown <68010199@kmitl.ac.th>
2026-02-20 18:14:38 +0700

    Updates

--- src/practicals/p05/payment-method.ts
+++ src/practicals/p05/payment-method.ts
@@ -1,1 +1,16 @@
-export class PaymentMethod {}
+import { Payable } from "./payable";
+
+export abstract class PaymentMethod implements Payable {
+  protected accountName: string;
+
+  constructor(accountName: string) {
+    this.accountName = accountName;
+  }
+
+  public getAccountName(): string {
+    return this.accountName;
+  }
+
+  abstract processPayment(amount: number): boolean;
+  abstract getPaymentFee(amount: number): number;
+}
