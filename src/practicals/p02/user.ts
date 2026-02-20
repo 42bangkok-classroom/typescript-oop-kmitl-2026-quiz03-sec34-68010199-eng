62dcb7b8186e16f181b8036fed1fd37c4fb781c1
unknown <68010199@kmitl.ac.th>
2026-02-20 17:58:35 +0700

    Updates

--- src/practicals/p02/user.ts
+++ src/practicals/p02/user.ts
@@ -1,1 +1,27 @@
-export class User {}
+export class User {
+  private firstname: string = "";
+  private lastname: string = "";
+  private age: number = 0;
+
+  static BIRTH_YEAR: number = 2000;
+
+  public setFirstname(firstname: string): void {
+    this.firstname = firstname;
+  }
+
+  public setLastname(lastname: string): void {
+    this.lastname = lastname;
+  }
+
+  public setAge(age: number): void {
+    this.age = age;
+  }
+
+  public getAge(): number {
+    return this.age;
+  }
+
+  public getFullName(): string {
+    return `${this.firstname} ${this.lastname}`;
+  }
+}
